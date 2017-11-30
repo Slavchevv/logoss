72773f82b44b703320b42bd7b93b62231783e88d
slavchevv <tsvetan.slavchev@gmail.com>
2017-11-30 21:48:27 +0100

    upload

---
+++ app/Http/Controllers/UploadController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Contracts\Filesystem\Filesystem;
+use Storage;
+class UploadController extends Controller
+{
+
+    public function index()
+    {
+        return view('upload');
+    }
+
+    public function imageUpload()
+    {
+        return view('upload-file');
+    }
+
+
+    public function imageUploadPost(Request $request)
+    {
+        $this->validate($request, [
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'text_file' => 'required|max:2048',
+
+        ]);
+
+        $imageName = time().'.'.$request->image->getClientOriginalExtension();
+        $textFileName = time().'.'.$request->text_file->getClientOriginalExtension();
+        $image = $request->file('image');
+        $text_file = $request->file('text_file');
+        $t = Storage::disk('s3')->put($imageName, file_get_contents($image), 'public');
+        $tf = Storage::disk('s3')->put($textFileName, file_get_contents($text_file), 'public');
+        $imageName = Storage::disk('s3')->url($imageName);
+        $textFileName = Storage::disk('s3')->url($textFileName);
+        return back()
+            ->with('success','Image Uploaded successfully.')
+            ->with('path',$imageName);
+    }
+
+    /*echo 123;
+       $s3 = Storage::disk('s3');
+       $s3->put('myfile.txt','my file!','public');*/
+}
